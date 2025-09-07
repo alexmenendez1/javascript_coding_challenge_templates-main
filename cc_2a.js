9cdd2b22e2471ae8d836ebc98c643a1d8137f3ae
alexmenendez1 <amenendez15@usf.edu>
2025-09-07 19:34:38 -0400

    Add files via upload

--- cc_2a.js
+++ cc_2a.js
@@ -1,12 +1,12 @@
 // Coding Challenge 2a
 
 // Code goes here
-let productName = 'Product X'
-let costPerUnit= 5
-let basePrice = 10
-let discountRate= .15
-let salesTaxRate =.007
-let fixedMonthlyCosts = 100
+let productName = 'Product X';
+let costPerUnit= 5;
+let basePrice = 10;
+let discountRate= .15;
+let salesTaxRate =.007;
+let fixedMonthlyCosts = 100;
 
 let discountedPrice = basePrice * (1-discountRate)
 let finalPriceWithTax = discountedPrice * (1+salesTaxRate)
@@ -14,6 +14,5 @@ let profitPerUnit = finalPriceWithTax - costPerUnit
 let breakEvenUnits = Math.ceil (fixedMonthlyCosts / profitPerUnit)
 let isProfitablePerUnit = profitPerUnit > 0
 
-console.log("Your Product is/n Your discounted price is/n Your final price with tax is/n Your profit per unit is /n Your breakeven units is/n This item is)
-
+console.log('Your Product is $${productName} /n Your discounted price is$${discountedPrice} /n Your final price with tax is $${finalPriceWithTax} /n Your profit per unit is $${profitPerUnit} /n Your breakeven units is $${breakEvenUnits} /n This item is$${isProfitablePerUnit}')
 
